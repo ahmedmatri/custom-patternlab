8d7c4eec3e242a05b624c62029a4b6f145f5fb49
bpalhbku <bpal@hbku.edu.qa>
2023-02-26 14:15:33 +0300

    added research tab

--- components/assets/js/scripts.js
+++ components/assets/js/scripts.js
@@ -1,15 +1,44 @@
-/* vertical tabs 2 js start here */
+/* vertical tabs js start here */
 
-if ($(".verticaltabs2").length > 0) {
+if ($(".verticaltabs_clg").length > 0) {
     $(function() {
-      const tabLinks = $('.verticaltabs2 #tab-links li a');
+      const tabLinks = $('.verticaltabs_clg .tab-links li a');
       tabLinks.click(function(event) {
-        var $this = $(this);
         event.preventDefault();
-        $('.verticaltabs2 #tab-links a.active, .verticaltabs2 .verticaltabs2__cont.active').removeClass('active');
+        var $this = $(this);
+        
+        $('.verticaltabs_clg .tab-links a.active, .verticaltabs_clg .verticaltabs__cont.active').removeClass('active');
         $this.addClass('active');
         $($this.attr('href')).addClass('active');
       });
     });
   }
-  /* vertical tabs 2 js end here */
+
+  if ($(".verticaltabs_res").length > 0) {
+    $(function() {
+      const tabLinks = $('.verticaltabs_res .tab-links li a');
+      tabLinks.click(function(event) {
+        event.preventDefault();
+        var $this = $(this);
+        
+        $('.verticaltabs_res .tab-links a.active, .verticaltabs_res .verticaltabs__cont.active').removeClass('active');
+        $this.addClass('active');
+        $($this.attr('href')).addClass('active');
+      });
+    });
+  }
+
+  if ($(".verticaltabs_cen").length > 0) {
+    $(function() {
+      const tabLinks = $('.verticaltabs_cen .tab-links li a');
+      tabLinks.click(function(event) {
+        event.preventDefault();
+        var $this = $(this);
+        
+        $('.verticaltabs_cen .tab-links a.active, .verticaltabs_cen .verticaltabs__cont.active').removeClass('active');
+        $this.addClass('active');
+        $($this.attr('href')).addClass('active');
+      });
+    });
+  }
+  /* vertical tabs js end here */
