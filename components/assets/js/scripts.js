08f5e8d35cbe07d54984583ac64aae046d71accb
bpal <bpal@hbku.edu.qa>
2023-03-08 11:07:02 +0300

    revised entity tabs code

--- components/assets/js/scripts.js
+++ components/assets/js/scripts.js
@@ -31,49 +31,6 @@ $(window).scroll(function () {
 		$('header, .wrapper').removeClass("sticky");
 	}
 });
-/* vertical tabs js start here */
-if ($(".verticaltabs_clg").length > 0) {
-    $(function() {
-      const tabLinks = $('.verticaltabs_clg .tab-links li a');
-      tabLinks.click(function(event) {
-        event.preventDefault();
-        let $this = $(this);
-        
-        $('.verticaltabs_clg .tab-links a.active, .verticaltabs_clg .verticaltabs__cont.active').removeClass('active');
-        $this.addClass('active');
-        $($this.attr('href')).addClass('active');
-      });
-    });
-  }
-
-  if ($(".verticaltabs_res").length > 0) {
-    $(function() {
-      const tabLinks = $('.verticaltabs_res .tab-links li a');
-      tabLinks.click(function(event) {
-        event.preventDefault();
-        let $this = $(this);
-        
-        $('.verticaltabs_res .tab-links a.active, .verticaltabs_res .verticaltabs__cont.active').removeClass('active');
-        $this.addClass('active');
-        $($this.attr('href')).addClass('active');
-      });
-    });
-  }
-
-  if ($(".verticaltabs_cen").length > 0) {
-    $(function() {
-      const tabLinks = $('.verticaltabs_cen .tab-links li a');
-      tabLinks.click(function(event) {
-        event.preventDefault();
-        let $this = $(this);
-        
-        $('.verticaltabs_cen .tab-links a.active, .verticaltabs_cen .verticaltabs__cont.active').removeClass('active');
-        $this.addClass('active');
-        $($this.attr('href')).addClass('active');
-      });
-    });
-  }
-  /* vertical tabs js end here */
 
   /* Event slider js start here */
   if ($(".eventList__slide").length > 0) {
