68b98ed00011d9c51cfc4f9a3492b5225c5028b2
bpal <bpal@hbku.edu.qa>
2023-03-28 10:33:45 +0300

    working on timer

---
+++ components/assets/js/timer.js
@@ -0,0 +1,13 @@
+$('#countdown')
+  .countdown('2024/10/10')
+  .on('update.countdown', function (event) {
+    var $this = $(this).html(
+      event.strftime(
+        '' +
+          '<div class="countdown_list"> <span class="countdown--days number">%-d</span> <span class="countdown--days-text smalltext">day%!d</span></div>' +
+          '<div class="countdown_list"> <span class="countdown--hours number">%H</span> <span class="countdown--hours-text smalltext">Hours </span></div>' +
+          '<div class="countdown_list"> <span class="countdown--minutes number">%M</span> <span class="countdown--minutes-text smalltext"> Minutes </span></div>' +
+          '<div class="countdown_list"> <span class="countdown--seconds number">%S</span> <span class="countdown--seconds-text smalltext"> Seconds </span></div>',
+      ),
+    );
+  });
